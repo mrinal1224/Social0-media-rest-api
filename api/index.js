a3672516552eb883d50091577c65519490461878
mrinal1224 <iammrinalkc@gmail.com>
2021-08-20 03:29:40 +0530

    regiter and login routes created

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const helmet = require('helmet')
+const mongoose = require('mongoose')
+const userRoute = require('./routes/usersRoutes')
+const authRoute = require("./routes/authRoutes");
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} , ()=>{
+    console.log('connected to mongoDb')
+});
+
+//middleware
+
+app.use(express.json())
+app.use(morgan('common'))
+app.use(helmet())
+
+app.get('/' , (req,res)=>{
+    res.send('Welcome to Home Page')
+})
+
+app.use('/api/users' , userRoute)
+
+app.use("/api/auth", authRoute);
+
+app.listen(8800 , ()=>{
+    console.log("backend Server is Running")
+})
